491937ada15cba9ded43aee32111a7b71fc054f9
lorenzoblaise <lorenzoblaise@gmail.com>
2026-06-29 14:34:44 +0200

    ajout fonctions vides

---
+++ Classe.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HNI_TPmoyennes
+{
+    internal class Classe
+    {
+        private const int MAXELEVES = 30;
+        private const int MAXMATIERES = 10;
+
+        public String nomClasse {  get; private set; }
+        public List<Eleve> eleves { get; private set; }
+        public List<String> matieres { get; private set; }
+        public float Moyenne { get; private set; }
+
+
+        public Classe(String nom) 
+        {
+            this.nomClasse = nom;
+            this.eleves = new List<Eleve>();
+            this.matieres = new List<string>();
+            this.Moyenne = 0;
+        }
+
+        public void ajouterEleve(String prenom,String nom)
+        {
+            eleves.Add(new Eleve(prenom,nom));
+        }
+
+        public void ajouterMatiere(String nom)
+        {
+            matieres.Add(nom);
+        }
+
+        public float moyenneGeneral()
+        {
+            return Moyenne;
+        }
+
+        public float moyenneMatiere(int i)
+        {
+            return Moyenne;
+        }
+    }
+}
---
+++ Eleve.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HNI_TPmoyennes
+{
+    internal class Eleve
+    {
+
+        private const int MAXNOTES= 200;
+
+        public String prenom {  get; private set; }
+        public String nom {  get; private set; }
+        public float Moyenne { get; private set; }
+        public List<Note> notes { get; private set; }
+
+
+        public Eleve(String prenom, String nom)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.Moyenne = 0;
+            this.notes= new List<Note>();
+        }
+
+        public void ajouterNote(Note note)
+        {
+            notes.Add(note);
+        }
+
+        public float moyenneGeneral()
+        {
+            return Moyenne;
+        }
+
+        public float moyenneMatiere(int i)
+        {
+            return Moyenne;
+        }
+    }
+}
